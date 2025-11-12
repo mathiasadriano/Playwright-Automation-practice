fdb1865baa62b7bdd959ea0329c6a15d7ed022dc
Mathias Hidalgo <u202213222@upc.edu.pe>
2025-11-11 19:35:38 -0500

    feat: Add failed login test (TC-002)

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -15,4 +15,22 @@ test.describe('Pruebas de Login en quotes.toscrape.com', () => {
     await expect(page.locator('a[href="/logout"]')).toBeVisible();
   });
 
+  test('TC-002: Login fallido (credenciales incorrectas)', async ({ page }) => {
+    
+    await page.goto('http://quotes.toscrape.com/login');
+
+    await page.fill('input[name="username"]', 'admin'); 
+    await page.fill('input[name="password"]', 'contraseñaincorrecta'); 
+
+    await page.click('input[type="submit"][value="Login"]');
+
+    const errorMessage = page.locator('text=Invalid username or password');
+    await expect(errorMessage).toBeVisible();
+
+    await expect(page).toHaveURL('http://quotes.toscrape.com/login');
+
+    await expect(page.locator('a[href="/logout"]')).not.toBeVisible();
+  });
+  
+
 });
